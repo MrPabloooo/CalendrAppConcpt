c1a8003140c01887e143367f6b40351b4a138175
MrPabloooo <pablo.suszczynski.dev@gmail.com>
2026-09-02 22:08:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calendrappconcpt/MainActivity.kt
@@ -0,0 +1,274 @@
+package com.example.calendrappconcpt
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.calendrappconcpt.ui.theme.CalendrAppConcptTheme
+import java.time.LocalDate
+import java.time.format.TextStyle
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            val NowDate = LocalDate.now()
+
+
+            CalendrAppConcptTheme {
+                Calendar()
+            }
+        }
+    }
+}
+
+
+
+data class MonthData(
+    val name: String,
+    val month: Int,
+    val year: Int,
+    val days: Int
+)
+
+
+@Composable
+fun Calendar(
+
+) {
+    val now = LocalDate.now()
+
+    var selectedDay by remember {
+        mutableStateOf(LocalDate.now())
+
+    }
+
+    val months = (0..11).map { offset ->
+        val date = now.minusMonths(offset.toLong())
+
+        MonthData(
+            name = date.month.getDisplayName(
+                TextStyle.FULL_STANDALONE,
+                java.util.Locale.getDefault()
+            ),
+            year = date.year,
+            days = date.lengthOfMonth(),
+            month = date.monthValue
+        )
+    }
+    var currentMonth by remember {
+        mutableIntStateOf(0)
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        MonthScreen(
+            month = months[currentMonth],
+            onPrevious = {
+                if (currentMonth < months.lastIndex) {
+                    currentMonth++
+//                    selectedDay = LocalDate.of(months[currentMonth].year, months[currentMonth].month, selectedDay.dayOfMonth)
+
+                }
+            },
+            onNext = {
+                if (currentMonth > 0) {
+                    currentMonth--
+//                    selectedDay = LocalDate.of(months[currentMonth].year, months[currentMonth].month, selectedDay.dayOfMonth)
+                }
+
+            },
+            SelectedDayUnit = {
+                day, month, year ->
+                selectedDay = LocalDate.of(year, month, day)
+
+            },
+            selectedDay = selectedDay
+        )
+
+        Text(text = "Selected Day: ${selectedDay.dayOfMonth}/${selectedDay.monthValue}/${selectedDay.year}")
+
+    }
+
+
+
+
+}
+
+
+@Composable
+fun MonthScreen(
+    month: MonthData,
+    onPrevious: () -> Unit,
+    onNext: () -> Unit,
+    SelectedDayUnit: (day: Int, month: Int, year: Int) -> Unit,
+    selectedDay: LocalDate
+) {
+
+
+
+
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            IconButton(
+                onClick = {
+                    onPrevious()
+                },
+
+            ) {
+
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Previous Month"
+                )
+
+            }
+
+
+            if (month.year != LocalDate.now().year) {
+                Text(text = "${month.name} ${month.year} ")
+            }
+            else {
+                Text(text = "${month.name}")
+            }
+
+
+            IconButton(
+                onClick = {
+                    onNext()
+                },
+
+                ) {
+
+                Icon(
+                    imageVector = Icons.Default.ArrowForward,
+                    contentDescription = "Previous Month"
+                )
+
+            }
+
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+
+        Column(
+            modifier = Modifier.wrapContentWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+
+
+        for (weekStart in 1..month.days step 7) {
+
+
+            Row(
+            ) {
+                val endDay = minOf(weekStart + 7, month.days + 1)
+
+                for (day in weekStart until minOf(weekStart + 7, month.days + 1)) {
+                    Box(
+                        modifier = Modifier
+                            .clickable {
+                                SelectedDayUnit(day, month.month, month.year)
+                            }
+                            .padding(4.dp)
+//                            .size(60.dp)
+                            .weight(1f)
+                            .aspectRatio(1f)
+                            .background(color =
+                                if (LocalDate.now().dayOfMonth == day && LocalDate.now().monthValue == month.month && LocalDate.now().year == month.year) {
+                                    Color.Red
+                                }
+                                else {
+                                    if (selectedDay.dayOfMonth == day && selectedDay.monthValue == month.month && selectedDay.year == month.year) {
+                                        Color.Green
+                                    }
+                                    else {
+                                        Color.Blue
+                                    }
+                                }
+                                ),
+
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = day.toString())
+                    }
+                }
+
+                repeat(7 - (endDay - weekStart)) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f)
+                            .padding(4.dp)
+                    )
+                }
+            }
+        }
+            }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CalendrAppConcptTheme {
+        Calendar()
+    }
+}
